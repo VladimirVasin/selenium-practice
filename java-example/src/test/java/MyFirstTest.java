a72085a61dae3b63cb8c28b5a6e59bbec380698f
VladimirVasin <tushkn91@gmail.com>
2022-02-17 16:45:09 +0400

    Добавлено ожидание состояния clickable для запуска через firefox (предотвращает падение теста)

--- java-example/src/test/java/MyFirstTest.java
+++ java-example/src/test/java/MyFirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest extends TestBase {
@@ -9,6 +10,7 @@ public class MyFirstTest extends TestBase {
     public void firstTest() {
         driver.get("http://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
+        wait.until(elementToBeClickable(By.name("btnK")));
         driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
 
--- java-example/src/test/java/TestBase.java
+++ java-example/src/test/java/TestBase.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.edge.EdgeDriver;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-
 
     public static WebDriver driver;
     public static WebDriverWait wait;
