a6fe5bb7b17509faf7e5359540dfc7aae7f7eda1
VladimirVasin <tushkn91@gmail.com>
2022-03-18 18:42:40 +0400

    Задание 12 исправление

--- java-example/src/test/java/Task12AddCatalog.java
+++ java-example/src/test/java/Task12AddCatalog.java
@@ -57,19 +57,20 @@ public class Task12AddCatalog {
                 driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog");
                 login();
                 driver.navigate().to("http://" + host + ":" + port + "/litecart/admin/?category_id=0&app=catalog&doc=edit_product");
-                driver.findElement(By.cssSelector("label:nth-child(3) > input[type=radio]")).click();
+                driver.findElement(By.cssSelector("label:nth-child(3) > input[type=radio]")).click();  //селектор исправлен
                 driver.findElement(By.name("name[en]")).sendKeys(nameProduct);
                 driver.findElement(By.name("code")).sendKeys("12345");
-                driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(4) > td > div > table > tbody > tr:nth-child(2) > td:nth-child(1) > input[type=checkbox]")).click();
-                driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(4) > td > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > input[type=checkbox]")).click();
-                driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(7) > td > div > table > tbody > tr:nth-child(4) > td:nth-child(1) > input[type=checkbox]")).click();
+                driver.findElement(By.cssSelector("input[name=\"categories[]\"]")).click(); //селектор исправлен
+                driver.findElements(By.cssSelector("input[name=\"categories[]\"]")).get(1).click(); //селектор исправлен
+
+                driver.findElements(By.cssSelector("input[name=\"product_groups[]\"]")).get(0).click(); //селектор исправлен
 
                 driver.findElement(By.name("quantity")).clear();
                 driver.findElement(By.name("quantity")).click();
                 driver.findElement(By.name("quantity")).sendKeys("5");
 
                 driver.findElement(By.name("sold_out_status_id")).click();
-                driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(8) > td > table > tbody > tr > td:nth-child(4) > select > option:nth-child(3)")).click();
+                driver.findElement(By.cssSelector("select[name=sold_out_status_id] > option:nth-child(3)")).click(); //селектор исправлен
                 String pathPicture = getClass().getClassLoader().getResource("greenDuck.png").getPath().replace("/", "\\").substring(1);
                 driver.findElement(By.cssSelector("input[type=file]")).sendKeys(pathPicture);
                 driver.findElement(By.name("date_valid_from")).sendKeys("18032022");
@@ -78,7 +79,7 @@ public class Task12AddCatalog {
                 wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.name("manufacturer_id"))));
 
                 driver.findElement(By.name("manufacturer_id")).click();
-                driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(1) > td > select > option:nth-child(2)")).click();
+                driver.findElement(By.cssSelector("select[name=manufacturer_id] > option:nth-child(2)")).click(); //селектор исправлен
                 driver.findElement(By.name("keywords")).sendKeys("123456789");
                 driver.findElement(By.name("short_description[en]")).sendKeys("description_Duck");
                 driver.findElement(By.className("trumbowyg-editor")).sendKeys("test description");
@@ -91,7 +92,7 @@ public class Task12AddCatalog {
                 pPrice.clear();
                 pPrice.sendKeys("999");
                 driver.findElement(By.name("purchase_price_currency_code")).click();
-                driver.findElement(By.cssSelector("#tab-prices > table:nth-child(2) > tbody > tr > td > select > option:nth-child(2)")).click();
+                driver.findElement(By.cssSelector("select[name=purchase_price_currency_code] > option:nth-child(2)")).click();  //селектор исправлен
                 driver.findElement(By.name("gross_prices[USD]")).clear();
                 driver.findElement(By.name("gross_prices[USD]")).sendKeys("5");
                 driver.findElement(By.name("gross_prices[EUR]")).clear();
